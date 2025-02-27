1918809c2ea4464f94a9fe3c37c8661f14c5eeec
Leonid Tots <59145792+huksleva@users.noreply.github.com>
2025-02-27 14:23:11 +0300

    123

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+    int x, y;
+    printf("a=");
+    //scanf("%d", &a);
+    printf("b=");
+    //scanf("%d", &b);
+    //printf("a+b=%d", a + b);
+
+    return 0;
+}
