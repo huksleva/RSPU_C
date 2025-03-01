705e28135a68d795abbcc9e8b1b7d891931b6c95
Leonid Tots <59145792+huksleva@users.noreply.github.com>
2025-03-01 18:52:58 +0300

    123

--- main.c
+++ main.c
@@ -1,30 +1,34 @@
 #include<stdio.h>
 #include<math.h>
 
-double MyFun(double x) {
-    return (x * x + 3 * x - 5);
-}
-
-double fun(double x){
-    return exp()
-}
-
 int main() {
-    double a, b, n, h, sum;
-    a = 5;
-    b = 8;
-    n = 10;
-    h = (b - a) / n;
-    sum = 0.0;
-    for (int i = 1; i <= n; i++) {
-        sum += ((MyFun(a + h * i) - MyFun(a + h * (i - 1))) / 2 * (a + h * i - a + h * (i - 1)));
+    const int SIZE = 10;
+    int a[] = {5, 2, 8, 1, 3, 6, 4, 7, 9, 0};
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("%d ", a[i]);
     }
-    printf("MyFun: %lf", sum);
+    printf("\n");
 
-    sum = 0.0;
+    int temp;
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE - i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
 
-
-
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
 
     return 0;
 }
