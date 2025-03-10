18c24b2a90311251f6683b594cbcfd9ba748dfc8
Leonid Tots <59145792+huksleva@users.noreply.github.com>
2025-03-10 13:31:50 +0300

    123

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
-#include <malloc.h> //библиотека для взаимодействия с оперативной памятью
+#include <malloc.h>
 
-int main() { double *pointer1 = malloc(sizeof(double)), **pointer2 = malloc(sizeof(double)), ***pointer3 = malloc(sizeof(double)); pointer3 = &pointer2;pointer2 = &pointer1;printf("pointer3="); scanf("%lf", **pointer3); printf("pointer3=%lf\n", ***pointer3); free(**pointer3); free(*pointer3); free(pointer3); return 0; }
+int main() {
+
+    //Размер типа double умножаем на кол-во элементов массива. Явно задаём тип возвращаемого значения функции - double*
+    double *arr = (double*) malloc(10*sizeof(double));
+
+    for (double *i = arr; i < sizeof(arr)/sizeof(double*); i++){
+        printf("Enter %d element (double):" );
+        scanf("%lf", &arr[i]);
+    }
+
+    free(arr);
+    return 0;
+}
+//1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0
