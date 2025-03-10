c703dec1634ef33413c4c59ff58eca5618726002
Leonid Tots <59145792+huksleva@users.noreply.github.com>
2025-03-10 10:54:26 +0300

    123

--- main.c
+++ main.c
@@ -1,11 +1,4 @@
 #include <stdio.h>
+#include <malloc.h> //библиотека для взаимодействия с оперативной памятью
 
-int main() {
-    int a, b;
-    printf("a=");
-    scanf("%d", &a);
-    printf("b=");
-    scanf("%d", &b);
-    printf("a+b=%d", a + b);
-    return 0;
-}
+int main() { double *pointer1 = malloc(sizeof(double)), **pointer2 = malloc(sizeof(double)), ***pointer3 = malloc(sizeof(double)); pointer3 = &pointer2;pointer2 = &pointer1;printf("pointer3="); scanf("%lf", **pointer3); printf("pointer3=%lf\n", ***pointer3); free(**pointer3); free(*pointer3); free(pointer3); return 0; }
