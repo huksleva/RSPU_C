1ae6e984db8fd3e81bdcfd32d804ef7795cb11cf
Leonid Tots <59145792+huksleva@users.noreply.github.com>
2025-03-10 08:24:39 +0300

    123

--- main.c
+++ main.c
@@ -1,34 +1,11 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
 
 int main() {
-    const int SIZE = 10;
-    int a[] = {5, 2, 8, 1, 3, 6, 4, 7, 9, 0};
-    for (int i = 0; i < SIZE; i++)
-    {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-
-    int temp;
-    for (int i = 0; i < SIZE; i++)
-    {
-        for (int j = 0; j < SIZE - i - 1; j++)
-        {
-            if (a[j] > a[j + 1])
-            {
-                temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
-            }
-        }
-    }
-
-    for (int i = 0; i < SIZE; i++)
-    {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-
+    int a, b;
+    printf("a=");
+    scanf("%d", &a);
+    printf("b=");
+    scanf("%d", &b);
+    printf("a+b=%d", a + b);
     return 0;
 }
