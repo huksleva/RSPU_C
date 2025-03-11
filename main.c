774675c804f21fce1665907be4f172fb69baa629
Leonid Tots <59145792+huksleva@users.noreply.github.com>
2025-03-11 10:42:43 +0300

    Сделал лабу 2

--- main.c
+++ main.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 
 int main() {
+    const int numOfStrings = 5;
+    char *strings = (char*) malloc(numOfStrings * sizeof(char*));
 
-    //Размер типа double умножаем на кол-во элементов массива. Явно задаём тип возвращаемого значения функции - double*
-    double *arr = (double*) malloc(10*sizeof(double));
-
-    for (double *i = arr; i < sizeof(arr)/sizeof(double*); i++){
-        printf("Enter %d element (double):" );
-        scanf("%lf", &arr[i]);
+    for (int i = 0; i < numOfStrings; i++) {
+        free(&strings[i]);
     }
+    free(strings);
 
-    free(arr);
     return 0;
 }
-//1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0
