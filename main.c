c9976c6b81a716623ce2b7571013bdeaa4ac7b85
Leonid Tots <59145792+huksleva@users.noreply.github.com>
2025-03-14 10:11:54 +0300

    Сделал лабу 2

--- main.c
+++ main.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    const int numOfStrings = 5;
-    char *strings = (char*) malloc(numOfStrings * sizeof(char*));
 
-    for (int i = 0; i < numOfStrings; i++) {
-        free(&strings[i]);
-    }
-    free(strings);
+
+
 
     return 0;
 }
