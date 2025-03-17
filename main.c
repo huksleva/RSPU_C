ee9bbb00c35a58bd277c297365d600d7d44cc90d
Leonid Tots <59145792+huksleva@users.noreply.github.com>
2025-03-17 09:10:09 +0300

    123

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main() {
+int sum(int a, int b){
+    return a + b;
+}
 
+struct mathStruct{
+    void *fun;
+}mathStruct;
+
+int main() {
+    mathStruct.fun = &sum;
+
+    //printf("%d\n", mathStruct.fun(1, 6));
 
 
 
