69252b457f0c986ffe7b339f643f14a2d608e6b2
Leonid Tots <59145792+huksleva@users.noreply.github.com>
2025-03-27 14:13:48 +0300

    Сделал самостоятельную работу 1

--- main.c
+++ main.c
@@ -2,53 +2,30 @@
 #include <stdlib.h>
 #include <math.h>
 
-struct Book {
-    unsigned short SIZE;
-    char *title;
-    char *author;
-    unsigned short year;
+enum Seasons {
+    WINTER, SPRING, SUMMER, AUTUMN
 };
 
-void printbook(struct Book* book){
-    printf("author: ");
-    printf("%s", book->author);
-    printf("\ntitle: ");
-    printf("%s", book->title);
-    printf("\nyear: %hd\n", book->year);
-}
-
 int main() {
-    struct Book *arrBook = (struct Book*) malloc(3*sizeof(struct Book));
-    for (int i = 0; i < 3; i++) {
-        arrBook[i].SIZE = 50;
-        arrBook[i].author = (char*) malloc(arrBook[i].SIZE*(sizeof(char)));
-        arrBook[i].title = (char*) malloc(arrBook[i].SIZE*(sizeof(char)));
+    printf("Enter num of month (1-12):");
+    int m;
+    scanf("%d", &m);
+
+    if ((m == 12) || (m == 1) || (m == 2)) {
+        printf("WINTER\n");
+    }
+    else if ((m == 3) || (m == 4) || (m == 5)){
+        printf("SPRING\n");
+    }
+    else if ((m == 6) || (m == 7) || (m == 8)) {
+        printf("SUMMER\n");
+    }
+    else if ((m == 9) || (m == 10) || (m == 11)) {
+        printf("AUTUMN\n");
+    }
+    else {
+        printf("ERROR\n");
     }
 
-    // Вводим данные в структуру
-    for (int i = 0; i < 3; i++) {
-        printf("\nstruct number %d:\n", i+1);
-        printf("Enter autor (max 50 symbols):");
-        scanf("%s", arrBook[i].author);
-        printf("Enter title (max 50 symbols):");
-        scanf("%s", arrBook[i].title);
-        printf("Enter year:");
-        scanf("%hd", &arrBook[i].year);
-    }
-
-    // Через функцию printbook() выводим структуры на экран
-    printf("\n");
-    for (int i = 0; i < 3; i++) {
-        printf("Book number %d:\n", i+1);
-        printbook(&arrBook[i]);
-        printf("\n");
-    }
-
-
-    for (int i = 0; i < 3; i++){
-        free(arrBook[i].title);
-        free(arrBook[i].author);
-    }
-    free(arrBook);
     return 0;
 }
