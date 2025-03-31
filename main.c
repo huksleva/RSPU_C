85d8dc31fb0fccb67cebad9b828de74c3abcee7f
Leonid Tots <59145792+huksleva@users.noreply.github.com>
2025-03-31 12:39:44 +0300

    123

--- main.c
+++ main.c
@@ -1,31 +1,73 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+#include <complex.h>
 
-enum Seasons {
-    WINTER, SPRING, SUMMER, AUTUMN
-};
+typedef struct Complex {
+    double real;
+    double imag;
+}Complex;
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+
+    int sum = 1;
+    for (int i = 1; i <= n; i++) {
+        sum *= i;
+    }
+    return sum;
+}
+
+Complex powComplex(Complex z, int n) {
+
+    // Используем формулу Муавра. Для этого найдём угол поворота (Fi) фи и расстояние (r).
+    double r = sqrt(z.real * z.real + z.imag * z.imag); // Расстояние
+    double Fi = atan(z.imag / z.real); // Угол поворота
+
+    double r_n = pow(r, n); // Расстояние в степени n. Вычисляем сейчас, чтобы потом не вычислять два раза.
+    z.real = r_n * cos(n*Fi); // Вещественная часть
+    z.imag = r_n * sin(n*Fi); // Мнимая часть
+
+    return z;
+}
 
 int main() {
-    printf("Enter num of month (1-12):");
-    int m;
-    scanf("%d", &m);
+    printf("z = a + bi\nEnter the Real part:");
+    Complex z;
+    scanf("%lf", &z.real);
+    printf("Enter the Imaginary part:");
+    scanf("%lf", &z.imag);
+    printf("z = %lf + %lfi\n\n", z.real, z.imag);
 
-    if ((m == 12) || (m == 1) || (m == 2)) {
-        printf("WINTER\n");
+
+
+
+
+
+
+
+
+
+    
+    Complex c = {0, 0};
+    Complex c_stakan;
+    double fraction; // Дробная часть выражения
+    for (int i = 0; i <= 20; i++) {
+        fraction = 1.0 / factorial(i);
+        c_stakan = powComplex(z, i);
+        c.real += c_stakan.real * fraction;
+        c.imag += c_stakan.imag * fraction;
     }
-    else if ((m == 3) || (m == 4) || (m == 5)){
-        printf("SPRING\n");
-    }
-    else if ((m == 6) || (m == 7) || (m == 8)) {
-        printf("SUMMER\n");
-    }
-    else if ((m == 9) || (m == 10) || (m == 11)) {
-        printf("AUTUMN\n");
-    }
-    else {
-        printf("ERROR\n");
-    }
+
+    printf("e^(i*Fi) = %lf + %lfi\n", c.real, c.imag);
+
+    double complex x = 1 + 2*I;
+
+    printf("\n\nLOOOOL: %lf", 8);
+
+
+
 
     return 0;
 }
