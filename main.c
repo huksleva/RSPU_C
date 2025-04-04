b68d809bcb47e65fbe0513efa9b41c5f01b3c56f
Leonid Tots <59145792+huksleva@users.noreply.github.com>
2025-04-04 20:30:00 +0300

    Закончил лабу 4

--- main.c
+++ main.c
@@ -1,45 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <ctype.h>
 
-
-typedef struct MyStruct{
-    union ASCII{
-        unsigned short num;
-        char sym;
-    }ASCII;
-
-    enum Indicator{
-        SYMBOL,
-        NUMBER
-    }Indicator;
-
-}MyStruct;
-
-void printMyStruct(MyStruct *s, int size) {
-    for (int i = 0; i < size; i++) {
-        if (s[i].Indicator == SYMBOL) {
-            printf("Struct %d: SYMBOL = %c\n", i+1, s[i].ASCII.sym);
-        } else if (s[i].Indicator == NUMBER) {
-            printf("Struct %d: NUMBER = %hu\n", i+1, s[i].ASCII.num);
-        }
-    }
-}
+#define SIZE 20
 
 int main() {
-    MyStruct *StructArr = (MyStruct *) malloc(sizeof(MyStruct) * 3);
+    char str[SIZE] = "Leonid Tots 2005";
 
-    StructArr[0].Indicator = NUMBER;
-    StructArr[0].ASCII.num = 77;
-
-    StructArr[1].Indicator = SYMBOL;
-    StructArr[1].ASCII.sym = 'a';
-
-    StructArr[2].Indicator = NUMBER;
-    StructArr[2].ASCII.num = 97;
-
-    printMyStruct(StructArr, 3);
-
-    free(StructArr);
+    for (int i = 0; i < strlen(str); i++) {
+        printf("Symbol '%c':\n", str[i]);
+        printf("   isalnum: %d\n", isalnum(str[i]));
+        printf("   isalpha: %d\n", isalpha(str[i]));
+        printf("   islower: %d\n", islower(str[i]));
+        printf("   isupper: %d\n", isupper(str[i]));
+        printf("   isdigit: %d\n", isdigit(str[i]));
+        printf("   isxdigit: %d\n", isxdigit(str[i]));
+        printf("   iscntrl: %d\n", iscntrl(str[i]));
+        printf("   isgraph: %d\n", isgraph(str[i]));
+        printf("   isspace: %d\n", isspace(str[i]));
+        printf("   isblank: %d\n", isblank(str[i]));
+        printf("   isprint: %d\n", isprint(str[i]));
+        printf("   ispunct: %d\n", ispunct(str[i]));
+    }
 
     return 0;
 }
